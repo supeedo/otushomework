d71f49fd1dc03a4cdd9b5e8b3bea8e76a03437b5
Sergey Yakovlev <web-wind@ya.ru>
2020-08-11 11:24:46 +0300

    DI

--- src/main/java/com/otushomework215/otushomework/base/BaseTest.java
+++ src/main/java/com/otushomework215/otushomework/base/BaseTest.java
@@ -6,22 +6,30 @@ import com.otushomework215.otushomework.configuration.Config;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+
 import java.util.concurrent.TimeUnit;
 
 
 public class BaseTest {
 
-    protected WebDriverFactory driverFactory = new WebDriverFactory();
+    @Autowired
+    protected WebDriverFactory driverFactory;
     public WebDriver driver;
     protected Config cfg = ConfigFactory.create(Config.class);
 
     @BeforeTest
     protected void setUp() {
         String browserType = System.getProperty("browser");
+        ApplicationContext javaConfigContext =
+                new AnnotationConfigApplicationContext(WebDriverFactory.class);
+        driverFactory = javaConfigContext.getBean(WebDriverFactory.class);
 
         if (browserType == null) driver = driverFactory.createNewDriver("chrome");
         else driver = driverFactory.createNewDriver(browserType);
